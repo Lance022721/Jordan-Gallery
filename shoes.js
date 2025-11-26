def91869897198a0d7bacf830cfbc2f7e916ea82
Lance022721 <lanceaustria2627@gmail.com>
2025-11-26 11:34:38 +0800

    Add initial HTML structure and JavaScript for Jordan shoe gallery with images

---
+++ shoes.js
@@ -0,0 +1,65 @@
+var classes = [
+    {
+        name: "Air Jordan 1 Black/Red (Bred)",
+        image: "images/j1b.jpg",
+        releaseDate: "September  1985",
+        description:
+            "The “banned” shoe that broke NBA uniform rules and sparked Jordan Brand’s legend.",
+    },
+    {
+        name: "Air Jordan 1 Chicago",
+        image: "images/j1.jpg",
+        releaseDate: "1985",
+        description:
+            "Worn heavily during MJ’s rookie season; the true starting point of the Air Jordan legacy.",
+    },
+    {
+        name: "Air Jordan 7 Olympic",
+        image: "images/j7.jpg",
+        releaseDate: "Summer 1992",
+        description:
+            "Worn by MJ and the Dream Team in the Olympics—global basketball domination.",
+    },
+    {
+        name: "Air Jordan 8 Playoff",
+        image: "images/j8.jpg",
+        releaseDate: "Febuary 1993",
+        description:
+            "Worn during his first three-peat, remembered for its bold straps and dominant playoff run.",
+    },
+    {
+        name: "Air Jordan 11 Concord",
+        image: "images/j11.jpg",
+        releaseDate: "November 1995",
+        description:
+            "Patent leather revolution; MJ wore these in his explosive 1995 return.",
+    },
+    {
+        name: "Air Jordan 11 Space Jam",
+        image: "images/j11s.jpg",
+        releaseDate: "December 2000 (but MJ wore them in 1995)",
+        description:
+            "Popularized by Space Jam; secretly worn in 1995 playoffs before public release.",
+    },
+    {
+        name: "Air Jordan 12 Flu Game",
+        image: "images/j12.jpg",
+        releaseDate: "1997",
+        description:
+            "The legendary “Flu Game”—Jordan scored 38 while sick in the 1997 Finals.",
+    },
+    {
+        name: "Air Jordan 14 Last Shot",
+        image:"images/j14.jpg",
+        releaseDate: "1998",
+        description:
+            "The shoe MJ wore while hitting “The Last Shot” in the 1998 Finals—his final Bulls moment.",
+    },
+    {
+        name: "Air Jordan 18 Sports Royal",
+        image: "images/j18.jpg",
+        releaseDate: "Febuary 2003",
+        description:
+            "MJ’s final NBA-game shoe, symbolizing the true end of his career.",
+    },
+];
